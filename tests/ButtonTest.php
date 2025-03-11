5b7e8f6c7df147f06aaafdd565add1a0f627da26
Shift <shift@laravelshift.com>
2025-03-11 16:45:28 +0000

    Shift to PSR-4 namespace

--- tests/ButtonTest.php
+++ tests/ButtonTest.php
@@ -2,7 +2,7 @@
 
 use Laravolt\SemanticForm\Elements\Button;
 
-class ButtonTest extends PHPUnit_Framework_TestCase
+class ButtonTest extends PHPUnit\Framework\TestCase
 {
     public function testButtonCanBeCreated()
     {
--- tests/PasswordTest.php
+++ tests/PasswordTest.php
@@ -2,7 +2,7 @@
 
 use Laravolt\SemanticForm\Elements\Password;
 
-class PasswordTest extends PHPUnit_Framework_TestCase
+class PasswordTest extends PHPUnit\Framework\TestCase
 {
 	public function testPasswordCanBeCreated()
 	{
